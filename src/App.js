592dfc31e24d497dfe57af8b475552e10752be52
Jeremy Roussel <jeremy.roussel13@gmail.com>
2020-08-22 21:21:19 -0500

    Front-End CRUD app with database mirrored to local state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {Container, Row, Col} from 'react-bootstrap'
+import FormModal from './components/modals/Modal'
+import DataTable from './components/tables/DataTable'
+import {connect} from 'react-redux'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      items: []
+    }
+  }
+
+  getItems(){
+    fetch('http://localhost:3001/crud')
+      .then(response => response.json())
+      .then(items => this.setState({items}))
+      .catch(err => console.log(err))
+  }
+
+  addItemToState = (item) => {
+    this.setState(prevState => ({
+      items: [...prevState.items, item]
+    }))
+  }
+
+  updateState = (item) => {
+    const itemIndex = this.state.items.findIndex(data => data.id === item.id)
+    const newArray = [
+      ...this.state.items.splice(itemIndex, 1)
+    ]
+    this.setState({ items: newArray })
+  }
+
+  deleteItemFromState = (id) => {
+    const updatedItems = this.state.items.filter(item => item.id !== id)
+    this.setState({ items: updatedItems })
+  }
+
+  componentDidMount(){
+    this.getItems()
+  }
+
+  render() {
+    return (
+      <Container className="App">
+        <Row>
+          <Col>
+            <h1 style={{margin: "20px 0"}}>CRUD Database - Mirrored to Local State</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <DataTable items={this.state.items} updateState={this.updateState} deleteItemFromState={this.deleteItemFromState} />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <FormModal buttonLabel="Add Item" addItemToState={this.addItemToState}/>
+          </Col>
+        </Row>
+      </Container>
+    )
+  }
 }
 
-export default App;
+export default App
